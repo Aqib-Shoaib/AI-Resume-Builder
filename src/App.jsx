901c69face08b26e44fe2b0cfda17b5f57906a70
Aqib-Shoaib <bluepitviper1233@gmail.com>
2025-01-04 14:23:21 +0500

    project setup completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      Hello World
+      <Button>Hn Bhai!</Button>
+    </>
+  );
+}
+
+export default App;
