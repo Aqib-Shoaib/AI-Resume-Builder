346b7670d101d60aaa3d2b4b0d667621f9a1bf9a
Aqib-Shoaib <bluepitviper1233@gmail.com>
2025-01-14 19:36:46 +0500

    gemini api integrated

--- src/dashboard/resume/components/forms/SummaryForm.jsx
+++ src/dashboard/resume/components/forms/SummaryForm.jsx
@@ -7,22 +7,39 @@ import GlobalApi from "./../../../../../services/GlobalApi";
 import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 import { Brain, LoaderCircle } from "lucide-react";
+import { ChatSession } from "./../../../../../services/GeminiApi";
+
+const PROMPT =
+  "Job Title: {JOBTITLE}; Give me 4-5 lined summary as per Job title ready to add in a resume for job application in JSON format with fields experienceLevel and summary, experience levels are fresher, mid-level and experienced";
 
 function SummaryForm({ enableNext }) {
   const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext);
   const [summary, setSummary] = useState();
   const [loading, setLoading] = useState();
   const params = useParams();
+  const [loading2, setLoading2] = useState(false);
+  const [summaryList, setSummaryList] = useState();
 
-  const handleInputChange = (e) => {
+  const handleInputChange = (summ) => {
     enableNext(false);
-    const summ = e.target;
 
     setSummary(summ);
     setResumeInfo({
       ...resumeInfo,
       summary: summ,
     });
+  };
+
+  const handleGenerate = async () => {
+    setLoading2(true);
+    const prompt = PROMPT.replace("{JOBTITLE}", resumeInfo?.jobTitle);
+    console.log(prompt);
+    const result = await ChatSession.sendMessage(prompt);
+
+    const list = JSON.parse(result.response.text());
+    console.log(list);
+    setSummaryList(list);
+    setLoading2(false);
   };
 
   const onSave = (e) => {
@@ -61,20 +78,46 @@ function SummaryForm({ enableNext }) {
               size='sm'
               variant='outline'
               type='button'
+              onClick={handleGenerate}
+              disabled={loading2}
             >
-              <Brain /> Generate with AI
+              {loading2 ? <LoaderCircle className='animate-spin' /> : <Brain />}
+              Generate with AI
             </Button>
           </div>
 
-          <Textarea className='mt-5' required onChange={handleInputChange} />
+          <Textarea
+            className='mt-5'
+            required
+            onChange={(e) => handleInputChange(e.target.value)}
+            value={resumeInfo?.summary}
+          />
 
           <div className='mt-2 flex justify-end'>
-            <Button disabled={loading} type='submit'>
+            <Button disabled={loading || loading2} type='submit'>
               {loading ? <LoaderCircle className='animate-spin' /> : "Save"}
             </Button>{" "}
           </div>
         </form>
       </div>
+
+      {summaryList && (
+        <div>
+          <h2 className='font-bold text-lg'>Suggestions</h2>
+          {summaryList.map((item, index) => (
+            <div
+              key={index}
+              className='rounded-lg shadow-lg cursor-pointer p-2'
+              onClick={() => handleInputChange(item?.summary)}
+            >
+              <h2 className='font-bold my-1 capitalize'>
+                {item?.experienceLevel}
+              </h2>
+              <p className='text-xs'>{item?.summary}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
